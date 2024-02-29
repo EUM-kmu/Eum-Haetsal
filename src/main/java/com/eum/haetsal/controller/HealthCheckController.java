795877edb32d3f437a54abd0c5779dec96e4e191
jykim-rust <alexj99@naver.com>
2024-02-29 17:58:26 +0900

    impl feat#1, #2

--- src/main/java/com/eum/haetsal/controller/HealthCheckController.java
+++ src/main/java/com/eum/haetsal/controller/HealthCheckController.java
@@ -1,6 +1,10 @@
 package com.eum.haetsal.controller;
 
+import com.eum.haetsal.service.BankService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +13,5 @@ public class HealthCheckController {
     public String healthCheck(){
         return "ok";
     }
+
 }
