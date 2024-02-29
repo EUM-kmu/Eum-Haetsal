49829cc455d5b482112e3ef422c02fedf12f4865
jykim-rust <alexj99@naver.com>
2024-02-29 17:58:26 +0900

    impl feat#1, #2

--- src/main/java/com/eum/haetsal/EumHaetsalApplication.java
+++ src/main/java/com/eum/haetsal/EumHaetsalApplication.java
@@ -1,7 +1,9 @@
 package com.eum.haetsal;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableFeignClients
 public class EumHaetsalApplication {
 
     public static void main(String[] args) {
@@ -22,6 +25,11 @@ public class EumHaetsalApplication {
                 registry.addMapping("/**").allowedOrigins("*");
             }
         };
+    }
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 
 }
