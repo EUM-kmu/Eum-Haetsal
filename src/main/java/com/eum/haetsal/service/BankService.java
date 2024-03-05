465ead3b718f6df54dce1b575582816cf97b734d
jykim-rust <alexj99@naver.com>
2024-03-05 20:10:30 +0900

    :sparkles: #5 뱅크와 연결을 위한 feignClient 응답 및 Exception handling

--- src/main/java/com/eum/haetsal/service/BankService.java
+++ src/main/java/com/eum/haetsal/service/BankService.java
@@ -2,8 +2,18 @@ package com.eum.haetsal.service;
 
 import com.eum.haetsal.client.BankClient;
 import com.eum.haetsal.common.DTO.APIResponse;
+import com.eum.haetsal.common.DTO.enums.SuccessCode;
 import com.eum.haetsal.controller.DTO.request.AccountRequestDTO;
 import com.eum.haetsal.controller.DTO.response.AccountResponseDTO;
+import com.eum.haetsal.controller.DTO.response.TotalTransferHistoryResponseDTO;
+import com.eum.haetsal.exception.BlockAccountException;
+import com.eum.haetsal.exception.FeignClientException;
+import com.eum.haetsal.exception.InsufficientAmountException;
+import com.eum.haetsal.exception.WrongPasswordException;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.ws.rs.BadRequestException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +35,57 @@ public class BankService {
         return bankClient.healthCheck().getBody();
     }
 
+    // 계좌 조회
+//    @ApiResponse(responseCode = "200", description = "성공"),
+//    @ApiResponse(responseCode = "400", description = "요청 형식 혹은 요청 콘텐츠가 올바르지 않을 때,",content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+//    @ApiResponse(responseCode = "401", description = "비밀번호 인증 에러 또는 거래상태 비적합",content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+//    @ApiResponse(responseCode = "402", description = "금액 부족",content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+//    @ApiResponse(responseCode = "403", description = "block된 계좌", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+//    @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     public APIResponse<AccountResponseDTO.AccountInfo> getAccountInfo(String accountNumber, String password) {
-        return bankClient.getAccountInfo(accountNumber, password).getBody();
+        try {
+            APIResponse<AccountResponseDTO.AccountInfo> response = bankClient.getAccountInfo(accountNumber, password).getBody();
+            return response;
+        } catch (FeignClientException e) {
+            switch (e.getStatus()){
+                case 400:
+                    throw new IllegalArgumentException(e.getErrorForm().getReason());
+                case 401:
+                    throw new WrongPasswordException(e.getErrorForm().getReason());
+                case 402:
+                    throw new InsufficientAmountException(e.getErrorForm().getReason());
+                case 403:
+                    throw new BlockAccountException(e.getErrorForm().getReason());
+                default:
+                    throw new RuntimeException(e.getErrorForm().getReason());
+            }
+        }
+
+    }
+
+    public APIResponse<TotalTransferHistoryResponseDTO.GetTotalTransferHistory> transfer(String fromAccountNumber, String toAccountNumber, String password,Long amount) {
+        try {
+            AccountRequestDTO.Transfer transfer = AccountRequestDTO.Transfer.builder()
+                    .accountNumber(fromAccountNumber)
+                    .receiverAccountNumber(toAccountNumber)
+                    .password(password)
+                    .amount(amount)
+                    .build();
+            APIResponse<TotalTransferHistoryResponseDTO.GetTotalTransferHistory> response = bankClient.transfer(transfer).getBody();
+            return response;
+        } catch (FeignClientException e) {
+            switch (e.getStatus()){
+                case 400:
+                    throw new BadRequestException(e.getErrorForm().getReason());
+                case 401:
+                    throw new WrongPasswordException(e.getErrorForm().getReason());
+                case 402:
+                    throw new InsufficientAmountException(e.getErrorForm().getReason());
+                case 403:
+                    throw new BlockAccountException(e.getErrorForm().getReason());
+                default:
+                    throw new RuntimeException(e.getErrorForm().getReason());
+            }
+        }
     }
 }
