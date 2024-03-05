31fbf377aa1d520be2d816d0e8f0d1219836a93b
jykim-rust <alexj99@naver.com>
2024-03-05 20:10:30 +0900

    :sparkles: #5 뱅크와 연결을 위한 feignClient 응답 및 Exception handling

--- src/main/java/com/eum/haetsal/EumHaetsalApplication.java
+++ src/main/java/com/eum/haetsal/EumHaetsalApplication.java
@@ -1,8 +1,11 @@
 package com.eum.haetsal;
 
+import com.eum.haetsal.client.FeignClientExceptionErrorDecoder;
 import feign.Logger;
+import feign.codec.ErrorDecoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -30,6 +33,11 @@ public class EumHaetsalApplication {
     @Bean
     public Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public FeignClientExceptionErrorDecoder commonFeignErrorDecoder() {
+        return new FeignClientExceptionErrorDecoder();
     }
 
 }
