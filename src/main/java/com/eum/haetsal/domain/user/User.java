e21537b2a4923bb7f929881b8f2a88879cd56988
yeonsu <jack0810@kookmin.ac.kr>
2024-03-24 16:59:39 +0900

    feat repost

--- src/main/java/com/eum/haetsal/domain/user/User.java
+++ src/main/java/com/eum/haetsal/domain/user/User.java
@@ -1,9 +1,13 @@
 package com.eum.haetsal.domain.user;
 
 import com.eum.haetsal.common.BaseTimeEntity;
+import com.eum.haetsal.domain.report.Report;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.List;
 import lombok.*;
 
 @Entity
@@ -14,6 +18,7 @@ import lombok.*;
 @Setter
 public class User extends BaseTimeEntity {
     @Id
+    @Column(name = "user_id")
     private Long userId;
 
     @Column(length = 50)
@@ -21,6 +26,9 @@ public class User extends BaseTimeEntity {
 
     @Column
     private String accountPassword;
+
+    @OneToMany(mappedBy = "user")
+    private List<Report> reports;
 
     public static User toEntity(Long userId, String password) {
         return User.builder().userId(userId).accountPassword(password).build();
