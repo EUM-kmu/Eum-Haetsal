c43c86f701777775b69b6a487a4240ad08c10164
solhee-hwang <solhee0912@kookmin.ac.kr>
2024-05-08 22:18:44 +0900

    chore : dev

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ port=`lsof -i :8000 -t`
 kill -9 $port
 git checkout dev
 git pull origin dev
+cp src/main/resources/devapplication.yml src/main/resources/application.yml
 nohup ./gradlew bootRun > gradlew.log 2>&1 &
