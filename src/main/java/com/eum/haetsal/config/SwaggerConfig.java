7a04581f2b0b03a732070a0c23f82d9890d61e3f
solhee-hwang <solhee0912@kookmin.ac.kr>
2024-05-14 01:01:26 +0900

    chore :  스웨거 문서 분기

--- src/main/java/com/eum/haetsal/config/SwaggerConfig.java
+++ src/main/java/com/eum/haetsal/config/SwaggerConfig.java
@@ -1,18 +1,24 @@
 package com.eum.haetsal.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
+@OpenAPIDefinition
 public class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI  customOpenAPI(@Value("${openapi.service.url}") String url) {
         String securityJwtName = "JWT";
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("JWT");
         Components components = new Components().addSecuritySchemes(securityJwtName, new SecurityScheme()
@@ -23,6 +29,7 @@ public class SwaggerConfig {
 
 
         return new OpenAPI()
+                .servers(List.of(new Server().url(url)))
                 .addSecurityItem(securityRequirement)
                 .components(components)
                 .info(apiInfo());
@@ -30,8 +37,8 @@ public class SwaggerConfig {
 
     private Info apiInfo() {
         return new Info()
-                .title("Springdoc 테스트")
-                .description("Springdoc을 사용한 Swagger UI 테스트")
+                .title("햇살 서버 문서")
+                .description("햇살 서버 Swagger")
                 .version("1.0.0");
     }
 }
